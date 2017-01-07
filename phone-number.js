f2ea394f619c0d44d12b2c09e1829f40c0314142
sensei100 <jenanton01@gmail.com>
2017-01-07 11:35:31 -0500

    finished

--- phone-number.js
+++ phone-number.js
@@ -1,11 +1,25 @@
 var PhoneNumber = function(phone, number) {
   this.phone = phone;
   this.number = function(number) {
-    this.number = this.number.replace(/[^\d]/g, ''); 
+    var formattedNumber = this.phone.replace(/[^\d]+/g,'');
+    if(formattedNumber.length != 10){
+            if(formattedNumber.length == 11 && formattedNumber[0] == '1' && formattedNumber[1] == '1'){
+                return formattedNumber.substring(1,formattedNumber.length);
+            } else {
+                return "0000000000"
+              }
+        }
+        
+        return phone.replace(/[^\d]+/g,''); 
+  }
+
+  this.areaCode = function() {
+    return phone.substring(0,3);
+  }
+  this.toString = function() {
+    return "("+this.areaCode()+")"+" "+phone.substring(3,6)+"-"+phone.substring(6,10);
   }
 }
-
-
 
 
 module.exports = PhoneNumber;
